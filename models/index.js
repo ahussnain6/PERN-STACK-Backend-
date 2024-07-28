c41ab690db48899a15c49a312d1fc3f58c8a1989
Ali Hussnain <ahussnain67h@gmail.com>
2024-07-28 12:05:15 +0500

    second commit

--- models/index.js
+++ models/index.js
@@ -1,17 +1,11 @@
 const { Sequelize } = require('sequelize');
-const config = require('../config/config');
-// Load environment variables
-const env = process.env.NODE_ENV || 'development';
-const configEnv = config[env];
-const sequelize = new Sequelize(configEnv.database, configEnv.username, configEnv.password, {
-    host: configEnv.host,
-    dialect: configEnv.dialect,
-    logging: configEnv.logging,
+const databaseUrl = process.env.psql;
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
 });
 sequelize.authenticate()
     .then(() => console.log('Database connection established.'))
     .catch(err => console.error('Unable to connect to the database:', err));
-
 module.exports = {
     sequelize,
     Sequelize,
